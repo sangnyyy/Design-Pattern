8977bb0865eb85382623fc911309432e93850e26
Sangmin_Lee <dltkdals2202@gmail.com>
2020-02-05 00:37:13 +0900

    Singleton Pattern

---
+++ src/me/sml/singleton/Main.java
@@ -0,0 +1,11 @@
+package me.sml.singleton;
+
+public class Main {
+    public static void main(String[] args){
+        Person person = Person.getInstance("hello");
+        Person person2 = Person.getInstance("hello2");
+        System.out.println(person.getName());
+        System.out.println(person2.getName());
+        System.out.println(person.equals(person2));
+    }
+}
---
+++ src/me/sml/singleton/Person.java
@@ -0,0 +1,22 @@
+package me.sml.singleton;
+
+public class Person {
+    private static Person person;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    private Person(String name){
+        this.name = name;
+    }
+
+    public static Person getInstance(String name){
+        if(person != null){
+            return person;
+        }
+        person = new Person(name);
+        return person;
+    }
+}
